f700f565d1a2d665034e592c8c7720751fd44e0d
tonly <tonly@163.com>
2023-09-06 16:54:06 +0800

    fix: time

--- server/library/command/time.go
+++ server/library/command/time.go
@@ -10,91 +10,80 @@ func init() {
 	//time.Local = time.FixedZone("EST", -5*3600)
 }
 
-//ParseTimeFromString 根据字符串解析时间 必须是 2006/1/2 15:04:05 格式的字符串
-//
-//@param
-//@return
-func ParseTimeFromString(itime string) (time.Time, error) {
-	parse, err := time.Parse("2006/1/2 15:04:05", itime)
+//GetNowDate 年-月-日
+func NowDate() string {
+	tz, _ := time.LoadLocation("Local") //设置时区
+	return time.Unix(time.Now().In(tz).Unix(), 0).In(tz).Format("2006-01-02")
+}
+
+//GetNowDateHour 年-月-日 时:分:钞
+func NowDateHour() string {
+	tz, _ := time.LoadLocation("Local") //设置时区
+	return time.Unix(time.Now().In(tz).Unix(), 0).In(tz).Format("2006-01-02 15:04:05")
+}
+
+//ParseTimeFromString 根据字符串(年-月-日 时:分:钞)解析对应时间戳
+func ParseTimeFromString(data string) (int, error) {
+	tz, _ := time.LoadLocation("Local") //设置时区
+	t, err := time.Parse("2006-01-02 15:04:05", data)
 	if err != nil {
-		return parse, err
+		return 0, err
 	}
-	return parse, nil
+	return int(t.In(tz).Unix()), nil
 }
 
-//GetNowDate 年月日
-//
-//@param
-//@return
-func GetNowDate() string {
+//TransDateByTime 年-月-日
+func TransDateByTime(data int64) string {
+	tz, _ := time.LoadLocation("Local") //设置时区
+	return time.Unix(data, 0).In(tz).Format("2006-01-02")
+}
+
+//TransDateByTime 年-月-日 时:分:钞
+func TransDateHourByTime(itime int64) string {
+	tz, _ := time.LoadLocation("Local") //设置时区
+	return time.Unix(itime, 0).In(tz).Format("2006-01-02 15:04:05")
+}
+
+//WhichTimestamp 获取指定时间戳零点时间戳
+func WhichTimestamp(itime int64) int {
+	tz, _ := time.LoadLocation("Local") //设置时区
+	t := time.Unix(itime, 0)
+	return int(time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, tz).In(tz).Unix())
+}
+
+//TodayHMSTimestamp 获取今日时分秒时间戳
+func TodayHMSTimestamp(hour, min, sec int) int {
 	timeZone, _ := time.LoadLocation("Local") //设置时区
-	return time.Unix(time.Now().In(timeZone).Unix(), 0).In(timeZone).Format("2006-01-02")
+	t := time.Now().In(timeZone)
+	return int(time.Date(t.Year(), t.Month(), t.Day(), hour, min, sec, 0, t.Location()).In(timeZone).Unix())
 }
 
-//TranDateByTime 年月日
-//
-//@param
-//@return
-func TranDateByTime(itime int) string {
-	timeZone, _ := time.LoadLocation("Local") //设置时区
-	return time.Unix(int64(itime), 0).In(timeZone).Format("2006-01-02")
-}
-
-//GetNowDateHour 年月日-时分钞
-//
-//@param
-//@return
-func GetNowDateHour() string {
-	timeZone, _ := time.LoadLocation("Local") //设置时区
-	return time.Unix(time.Now().In(timeZone).Unix(), 0).In(timeZone).Format("2006-01-02 15:04:05")
-}
-
-//GetTodayTimestamp 获取今日零点时间戳
-//
-//@param
-//@return
-func GetTodayTimestamp() int {
+//TodayTimestamp 获取今日零点时间戳
+func TodayTimestamp() int {
 	timeZone, _ := time.LoadLocation("Local") //设置时区
 	t := time.Now().In(timeZone)
 	return int(time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location()).In(timeZone).Unix())
 }
 
-//GetWhichTimestamp 获取指定时间戳零点时间戳
-//
-//@param
-//@return
-func GetWhichTimestamp(itime int) int {
-	timeZone, _ := time.LoadLocation("Local") //设置时区
-	t := time.Unix(int64(itime), 0)
-	return int(time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, timeZone).In(timeZone).Unix())
+//TomorrowTimestamp 获取明天零点时间戳
+func TomorrowTimestamp() int {
+	tz, _ := time.LoadLocation("Local") //设置时区
+	t := time.Now().In(tz)
+	return int(time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location()).In(tz).Add(time.Second * 86400).In(tz).Unix())
 }
 
-//GetTodayHMSTimestamp 获取今日时分秒时间戳
-//
-//@param
-//@return
-func GetTodayHMSTimestamp(hour, min, sec int) uint32 {
-	timeZone, _ := time.LoadLocation("Local") //设置时区
-	t := time.Now().In(timeZone)
-	return uint32(time.Date(t.Year(), t.Month(), t.Day(), hour, min, sec, 0, t.Location()).In(timeZone).Unix())
-}
+// CurrentWeekSunday 本周周日时间戳(零点)
+// 一周的第一天为周一,最后一天为周日
+func CurrentWeekSunday() int {
+	now := time.Now()
+	today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local)
 
-//GetTomorrowHMSTimestamp 获取明日时分秒时间戳
-//
-//@param
-//@return
-func GetTomorrowHMSTimestamp(hour, min, sec int) uint32 {
-	timeZone, _ := time.LoadLocation("Local") //设置时区
-	t := time.Now().In(timeZone)
-	return uint32(time.Date(t.Year(), t.Month(), t.Day()+1, hour, min, sec, 0, t.Location()).In(timeZone).Unix())
-}
+	week := now.Weekday()
+	offDays := 7 - week // 距离周日相差几天
+	if week == time.Sunday {
+		offDays = 0
+	}
+	Sunday := today.Add(time.Duration(offDays) * 86400 * time.Second).In(time.Local)
 
-//GetTomorrowTimestamp 获取明天零点时间戳
-//
-//@param
-//@return
-func GetTomorrowTimestamp() int {
-	timeZone, _ := time.LoadLocation("Local") //设置时区
-	t := time.Now().In(timeZone)
-	return int(time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location()).In(timeZone).Add(time.Second * 86400).In(timeZone).Unix())
+	return int(Sunday.Unix())
 }
