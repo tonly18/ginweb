0f683fe4f70fcbac2ee3112c6fe07d73848feb66
tonly <tonly@163.com>
2023-09-07 15:59:22 +0800

    fix: http client

--- server/core/httpclient/client.go
+++ server/core/httpclient/client.go
@@ -2,7 +2,6 @@ package httpclient
 
 import (
 	"bytes"
-	"crypto/tls"
 	"fmt"
 	"github.com/spf13/cast"
 	"io"
@@ -25,21 +24,8 @@ func NewHttpClient(config *Config) *HttpClient {
 
 	return &HttpClient{
 		httpClient: &http.Client{
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: true, //不校验服务端证书
-				},
-				//ForceAttemptHTTP2: true, //强制使用http2(默认值:true)
-				//MaxIdleConns:      100, //最大空闲数量(默认值:100)
-				DisableKeepAlives: true,
-
-				//TLSHandshakeTimeout:   5 * time.Second,  //TLS握手超时(默认值:10)
-				//ResponseHeaderTimeout: 1 * time.Second,  //限制读取response header的时间
-				//ExpectContinueTimeout: 1 * time.Second,  //限制client在发送包含Expect:100-continue的header到收到继续发送body的response之间的时间等待。
-				//IdleConnTimeout:       60 * time.Second, //连接空闲超时
-				DisableCompression: true, //禁止压缩
-			},
-			Timeout: config.TimeOut, //从连接(Dial)到读完response body
+			Transport: transport,
+			Timeout:   config.TimeOut, //从连接(Dial)到读完response body
 		},
 		httpRequest: nil,
 	}
--- server/core/httpclient/config.go
+++ server/core/httpclient/config.go
@@ -1,11 +1,32 @@
 package httpclient
 
 import (
+	"crypto/tls"
 	"net/http"
 	"time"
 )
 
 type Config struct {
-	Transport http.RoundTripper
-	TimeOut   time.Duration
+	CheckRedirect func(req *http.Request, via []*http.Request) error
+	Jar           http.CookieJar
+	TimeOut       time.Duration
+}
+
+/*
+http.Transport内都会维护一个自己的空闲连接池,如果每个client都创建一个新的http.Transport,就会导致底层的TCP连接无法复用.
+如果网络请求过大,上面这种情况会导致协程数量变得非常多,导致服务不稳定.
+*/
+var transport = &http.Transport{
+	TLSClientConfig: &tls.Config{
+		InsecureSkipVerify: true, //不校验服务端证书
+	},
+	DisableKeepAlives:  true,
+	DisableCompression: true, //禁止压缩
+
+	//ForceAttemptHTTP2: true, //强制使用http2(默认值:true)
+	//MaxIdleConns:      100, //最大空闲数量(默认值:100)
+	//IdleConnTimeout:       60 * time.Second, //连接空闲超时
+	//TLSHandshakeTimeout:   5 * time.Second,  //TLS握手超时(默认值:10)
+	//ResponseHeaderTimeout: 1 * time.Second,  //限制读取response header的时间
+	//ExpectContinueTimeout: 1 * time.Second,  //限制client在发送包含Expect:100-continue的header到收到继续发送body的response之间的时间等待。
 }
